45173b8a82a714b9b4166e9bdfff4493e73b2b4d
prince raj <prince.raj@salescode.ai>
2024-10-27 15:16:35 +0530

    apply new theme

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="relative">
-        <img className="w-screen h-[400px] object-cover opacity-45	" src="https://img.freepik.com/free-photo/chocolate-cookies-glaze_23-2148837158.jpg?semt=ais_hybrid"/>
+      {/* <img className="w-screen h-[400px] object-cover opacity-45	" src="https://img.freepik.com/free-photo/chocolate-cookies-glaze_23-2148837158.jpg?semt=ais_hybrid"/>
         <div className="absolute bottom-16 text-center">
             <h1 className="font-bold">HEllo World</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia amet corporis provident, saepe asperiores, tenetur quisquam consectetur reprehenderit eos mollitia eaque. Possimus, odio neque. Veritatis officiis nihil qui corrupti id.</p>
-        </div>
+        </div> */}
     </div>
   );
 };
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -4,20 +4,21 @@ import { AppContext } from '../../context/myContext';
 import "./Navbar.css";
 
 const Navbar = () => {
-    const {showPopup, handlePopUpVisibility, hidePopup, arrowClick} = useContext(AppContext);
-    
+    const { showPopup, handlePopUpVisibility, hidePopup, arrowClick } = useContext(AppContext);
+
     return (
-        
+
         <div className='nav-container'>
-            {!showPopup? <div className='menu' onClick={handlePopUpVisibility}>
-                <svg className="nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </div>: <div className='menu' onClick={hidePopup}>
-            <svg className="nav-icon"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
-            </div>}
-            {arrowClick && <Popup/>}
-        
+            {!showPopup ?
+                <div className='menu' onClick={handlePopUpVisibility}>
+                    <svg className="nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </div> :
+                <div className='menu' onClick={hidePopup}>
+                    <svg className="nav-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+                </div>}
+            {arrowClick && <Popup />}
 
         </div>
     )
