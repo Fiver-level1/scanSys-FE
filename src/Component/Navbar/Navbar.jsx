93c78e19133ea7ba974c8d85b6d08b3774e89173
vanshika <vanshika.rajput@salescode.ai>
2024-10-27 09:36:40 +0530

    removed tailwind from nav and sidebar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React, { useContext } from 'react'
 import Popup from '../Popup/Popup';
 import { AppContext } from '../../context/myContext';
-import Sidebar from '../Sidebar/Sidebar';
+import "./Navbar.css";
 
 const Navbar = () => {
     const {showPopup, handlePopUpVisibility, hidePopup, arrowClick} = useContext(AppContext);
     
     return (
-        <div className='bg-[#354259] py-3 px-5	'>
-            {!showPopup? <div className='w-4' onClick={handlePopUpVisibility}>
-                <svg class="h-8 w-8 text-[#ECE5C7]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        
+        <div className='nav-container'>
+            {!showPopup? <div className='menu' onClick={handlePopUpVisibility}>
+                <svg className="nav-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
-            </div>: <div className='w-4' onClick={hidePopup}>
-            <svg class="h-8 w-8 text-[#ECE5C7]"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+            </div>: <div className='menu' onClick={hidePopup}>
+            <svg className="nav-icon"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
             </div>}
             {arrowClick && <Popup/>}
         
---
+++ src/assets/svg/arrow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Arrow = () => {
+  return (
+    <>
+        <svg class="h-6 w-6 text-[#354259]"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="9 18 15 12 9 6" /></svg>
+    </>
+  )
+}
+
+export default Arrow
