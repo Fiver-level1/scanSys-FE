029ff6095e484b85418ba8dec647298b6a9550bd
vanshika <vanshika.rajput@salescode.ai>
2024-11-01 00:58:43 +0530

    search

--- src/Component/Filter/FilterNav.jsx
+++ src/Component/Filter/FilterNav.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './filterNav.css'
 import { IoSearch } from "react-icons/io5";
+import { AppContext, AppDispatchContext } from '../../context/myContext';
 const FilterNav = () => {
+    const { productdata, searchValue } = useContext(AppContext);
+    const { setSearchValue } = useContext(AppDispatchContext);
+   
+    // console.log(productdata);
+
+    const handleSearchValue = (e)=>{
+        setSearchValue(e.target.value);
+    }
+
+    // console.log(searchValue);
+
+
     return (
         <nav className="FilterNavContainer">
             <div className="FilterNavWrapper">
                 <div className="field">
-                    <input required="" type="text" className="input" placeholder='Search' />
+                    <input required="" type="text" className="input" placeholder='Search' value={searchValue} onChange={handleSearchValue}/>
                     <span className="span">
                         <IoSearch />
                     </span>
                 </div>
                 <div className="filters">
-                    <li>shake</li>
-                    <li>chicken</li>
-                    <li>shake</li>
-                    <li>chicken</li>
-                    <li>shake</li>
-                    <li>chicken</li>
-                    <li>shake</li>
-                    <li>chicken</li>
-                    <li>shake</li>
-                    <li>chicken</li>
+                    {
+                        productdata.map((item, index) => {
+                            return <a href={`#${item.category}`}><li key={index} >{item.category}</li></a>
+                        })
+
+                    }
                 </div>
             </div>
         </nav>
