f59175503ba08b8fa58b32c1f00515251da9e6bb
vanshika <vanshika.rajput@salescode.ai>
2024-11-01 16:37:05 +0530

    debouncing

--- src/Component/Filter/FilterNav.jsx
+++ src/Component/Filter/FilterNav.jsx
@@ -1,26 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import './filterNav.css'
 import { IoSearch } from "react-icons/io5";
 import { AppContext, AppDispatchContext } from '../../context/myContext';
 const FilterNav = () => {
-
-    const { productdata, searchValue } = useContext(AppContext);
+    const { productdata } = useContext(AppContext);
     const { setSearchValue } = useContext(AppDispatchContext);
+    const [input, setInput] = useState("");
+   
+    // console.log(productdata);
 
     const handleSearchValue = (e)=>{
-        setSearchValue(e.target.value);
+        setInput(e.target.value);
     }
+
+    // console.log(searchValue);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setSearchValue(input);
+        }, 500); // 500ms delay
+
+        // Clear timeout if input changes again within the delay
+        return () => clearTimeout(timerId);
+    }, [input]);
+
 
     return (
         <nav className="FilterNavContainer">
             <div className="FilterNavWrapper">
                 <div className="field">
-                    <input required="" type="text" className="input" placeholder='Search' value={searchValue} onChange={handleSearchValue}/>
+                    <input required="" type="text" className="input" placeholder='Search' value={input} onChange={handleSearchValue}/>
                     <span className="span">
                         <IoSearch />
                     </span>
                 </div>
                 <div className="filters">
+                    {
+                        productdata.map((item, index) => {
+                            return <a href={`#${item.category}`}><li key={index} >{item.category}</li></a>
+                        })
+
+                    }
                 {
                         productdata.map((item, index) => {
                             return <a href={`#${item.category}`}><li key={index} >{item.category}</li></a>
