b976168bb3aacfd86009a00349fb8622bde69f5f
vanshika <vanshika.rajput@salescode.ai>
2024-11-09 19:25:36 +0530

    order history page done

--- src/Component/OrderHistory/OrderHistory.jsx
+++ src/Component/OrderHistory/OrderHistory.jsx
@@ -1,51 +1,70 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import "../FoodCardHome/listFoodCard.css"
+import "./OrderHistory.css";
+import { getRequestAuth } from '../../Services/AuthControllerWithoutToken';
+import { getRequest } from '../../Services/ApiController';
+import { AuthContext } from '../../context/AuthContext';
+import BackNavigate from '../BackNavgate/BackNavigate';
 
 const OrderHistory = () => {
+    const { isLogin } = useContext(AuthContext);
+    const [orderData, setOrderData] = useState([]);
+
+    useEffect(() => {
+        // if(!isLogin){
+        getRequest("/api/orders/", (error, response) => {
+            if (error) {
+
+            } else {
+                setOrderData(response.data)
+            }
+        })
+        // }
+    }, [])
+
     return (
-        <div>
-            <div className="headerPrimary">
-                <h1>Almost There – Your Feast Awaits!</h1>
-                <p>Review your delicious picks! Make sure everything looks perfect before you checkout. Add or adjust items to satisfy your cravings, and get ready for a quick and tasty delivery!</p>
-            </div>
-            <div className='foodCardHolder'>
-                <div className="foodCardWrapper">
+        <div className="cartContainer">
+            <div className="cartWrapper">
+                <BackNavigate />
+                <div className="headerPrimary">
+                    <h1>Almost There – Your Feast Awaits!</h1>
+                    <p>Review your delicious picks! Make sure everything looks perfect before you checkout. Add or adjust items to satisfy your cravings, and get ready for a quick and tasty delivery!</p>
+                </div>
+                <div className='foodCardHolder'>
 
-                    <div className="lisProductWrapper">
+                    <div className="foodCardWrapper">
+                        <div className="lisProductWrapper">
+                            {orderData.length > 0 && orderData.map((order, index) => {
+                                return (<div className="wrapper order-history-wrapper">
+                                    <div className='order-history-card' key={index}>
+                                        <p>{new Date(order?.created_at).toDateString()}</p>
 
-                        <div className="wrapper">
-                            <div className="firstArea">
-                                <div className="img-area">
-                                    <div className="inner-area">
-                                        {/* <img
-                            src={`${BASE_URL}${productData?.image}`}
-                            alt=""
-                        /> */}
+                                        <p>Order Number - {order?.id}</p>
+                                        <div className='allStatus'>
+                                            <div className='status'>
+                                                Order Status
+                                                <p
+                                                    // className={`${orderStatus}`}
+                                                    className={`${order.order_status.toLowerCase()}`}
+                                                >{order?.order_status}</p>
+                                            </div>
+                                            <div className={'status'}>
+                                                Payment Status
+                                                <p
+                                                    // className={`${orderStatus}`}
+                                                    className={`${order.payment_status.toLowerCase()}`}
+                                                >{order?.payment_status}</p>
+                                            </div>
+                                        </div>
+                                        <div className='border-order-card'></div>
+                                        <h5>${order.total_amount}</h5>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="secondArea">
-                                {/* <h2>{productData?.name}</h2>
-                <p>{productData?.description}</p> */}
-                                <div className="priceTag">
-                                    {/* <h1><MdOutlineEuroSymbol /> {productData?.price}</h1> */}
-                                    {
-                                        // Qty ?
-                                        //     <>
-                                        //         <span>|</span>
-                                        //         <h2>Qty : {Qty}</h2>
-                                        //     </>
-                                        //     :
-                                        //     <></>
-                                    }
-                                </div>
-                            </div>
 
-                            {/* <div> Order</div> */}
+                                </div>
+                                )
+                            })}
                         </div>
-
                     </div>
-
                 </div>
             </div>
         </div>
